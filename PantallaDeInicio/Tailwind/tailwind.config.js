f00f54a9b097d77a18b0421b01ac085698ac2237
Orzy895 <enrique.wen@utp.ac.pa>
2023-11-07 15:20:57 -0500

    +color-fondo/inicio-tailwind

--- PantallaDeInicio/Tailwind/tailwind.config.js
+++ PantallaDeInicio/Tailwind/tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
       '1/2': '50%',
       '3/4': '75%',
     },
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'radial-gradient': 'radial-gradient(86.94% 47.67% at 50.02% 52.33%, #431B19 35.42%, #141414 73.96%)',
+      },
+    },
   },
   plugins: [],
 }
